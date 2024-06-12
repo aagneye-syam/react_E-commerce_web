44c0ef28d0147839c203152e9a4ddb7ab1103976
aagneye-syam <saagneye2003@gmail.com>
2024-06-12 07:39:05 +0530

    some error resolved

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDom from 'react-dom'
-import App from './APP'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<APP />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
