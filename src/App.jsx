2c045f46869646aa6ccd142bfc0711b405ed5a4a
aagneye-syam <saagneye2003@gmail.com>
2024-06-19 23:13:56 +0530

    tailwind got reinitialized and the styling error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,22 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <div className="fixed bottom-4 right-4 z-index: 1000">
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                style={{ background: 'blue', borderRadius: '50%' }}
-                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                style={{
+                  background: "blue",
+                  borderRadius: "50%",
+                  padding: "5px",
+                }}
+                className="text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {/* {activeMenu ? <div>Sidebar</div> : <div>sidebar w-0</div>} */}
         </div>
       </BrowserRouter>
     </div>
