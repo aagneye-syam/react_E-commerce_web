13e8423366819efff6d1bac145c8e75964ad324b
aagneye-syam <saagneye2003@gmail.com>
2024-06-19 23:21:21 +0530

    activeMenu and useStateContext defined

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { useStateContext } from "./contexts/ContextProvider";
+
 import "./App.css";
 
 const App = () => {
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
