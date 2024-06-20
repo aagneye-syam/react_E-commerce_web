ab32c9a5706ce53126b96e0363c0f19a1d707f51
aagneye-syam <saagneye2003@gmail.com>
2024-06-20 23:06:20 +0530

    Navbar closing button added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,6 +27,16 @@ const Sidebar = () => {
             <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
               <SiShopware /> <span>Business Name</span>
             </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
           </div>
         </>
       )}
