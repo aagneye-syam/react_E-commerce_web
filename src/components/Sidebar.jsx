f57aae7226adebc4eb5e3f0bac4b605bd1c35f80
aagneye-syam <saagneye2003@gmail.com>
2024-06-20 23:30:53 +0530

    NavLink added to Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -45,9 +45,25 @@ const Sidebar = () => {
             </TooltipComponent>
           </div>
           <div className="mt-10">
-            {links.map((items) => (
-              <div className="text-grey-400 m-3 mt-4 uppercase">
-                {items.title}
+            {links.map((item) => (
+              <div key={item.title}>
+                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : '',
+                    })}
+                    className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                  >
+                    {link.icon}
+                    <span className="capitalize ">{link.name}</span>
+                  </NavLink>
+                ))}
               </div>
             ))}
           </div>
